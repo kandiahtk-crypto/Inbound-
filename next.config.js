fc2bd5fa3bf621c7ce96508fb210e48463ebcc99
kandiahtk-crypto <kandiah.tk@gmail.com>
2026-03-19 10:11:13 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
+
+---
+
+📄 postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+---
 
